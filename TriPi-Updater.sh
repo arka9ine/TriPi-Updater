e57c58d3584fe9fff24ba6db8b1eea0816d43eda
pancakeykakes <thephoenixproject89@gmail.com>
2015-10-25 20:52:00 -0400

    Update TriPi-Updater.sh
    Final Touches

--- TriPi-Updater.sh
+++ TriPi-Updater.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
- cd
- sudo apt-get -y install git
- sudo rm -rf TriPi
- sudo rm -rf TriPi-Updater
- sudo git clone https://github.com/pancakeykakes/TriPi-Updater.git
- cd TriPi-Updater
- sudo chmod +x update.sh
- sudo ./update.sh
+sudo killall -9 retroarch
+sudo killall -9 kodi.bin
+sudo killall emulationstation
+(sleep 1)
+echo "$(tput setaf 3)Checking environment and Updatiing TriPi...$(tuput sgr 0)"
+cd
+sudo apt-get -y install git
+sudo rm -rf TriPi
+sudo rm -rf TriPi-Updater
+sudo git clone https://github.com/pancakeykakes/TriPi-Updater.git
+cd TriPi-Updater
+sudo chmod +x update.sh
+sudo ./update.sh
