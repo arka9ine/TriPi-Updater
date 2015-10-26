c4c4037cd4b114ab6ac6d07eac0243d739ccde80
pancakeykakes <thephoenixproject89@gmail.com>
2015-10-26 15:12:37 -0400

    Update TriPi-Updater.sh
    Typo fix

--- TriPi-Updater.sh
+++ TriPi-Updater.sh
@@ -3,7 +3,7 @@ sudo killall -9 retroarch
 sudo killall -9 kodi.bin
 sudo killall emulationstation
 (sleep 1)
-echo "$(tput setaf 3)Checking environment and Updatiing TriPi...$(tuput sgr 0)"
+echo "$(tput setaf 3)Checking environment and Updatiing TriPi...$(tput sgr 0)"
 cd
 sudo apt-get -y install git
 sudo rm -rf TriPi
