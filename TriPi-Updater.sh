9490bc5a6ec60c7f9b08048fa1e1802b3d0a9b1e
pancakeykakes <thephoenixproject89@gmail.com>
2015-10-25 20:44:24 -0400

    Update TriPi-Updater.sh
    Final touches & typo fix

--- TriPi-Updater.sh
+++ TriPi-Updater.sh
@@ -3,7 +3,7 @@
  sudo apt-get -y install git
  sudo rm -rf TriPi
  sudo rm -rf TriPi-Updater
- sudo git clone https://github.com/pancakeykakes/TriPi-updater.git
- cd TriPi-updater
+ sudo git clone https://github.com/pancakeykakes/TriPi-Updater.git
+ cd TriPi-Updater
  sudo chmod +x update.sh
  sudo ./update.sh
