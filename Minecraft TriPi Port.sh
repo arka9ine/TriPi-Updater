b7d7642d75cc1a1708d38662ce3a5297792049b3
pancakeykakes <thephoenixproject89@gmail.com>
2015-11-13 23:15:41 -0500

    Update Minecraft TriPi Port.sh

--- Minecraft TriPi Port.sh
+++ Minecraft TriPi Port.sh
@@ -4,8 +4,8 @@ sudo killall xboxdrv
         sleep .2
         xboxdrv \
          --ui-axismap x2=REL_X:10,y2=REL_Y:10,x1=KEY_LEFT:KEY_RIGHT,y1=KEY_UP:KEY_DOWN,x1=KEY_A:KEY_D,y1=KEY_W:KEY_S \
-         --ui-buttonmap RT=BTN_RIGHT,LT=BTN_LEFT,x=KEY_E \
-         --ui-buttonmap A=KEY_SPACE --ui-buttonmap B=KEY_LEFTSHIFT --ui-buttonmap X=KEY_E --ui-buttonmap BACK=KEY_ESC --ui-buttonmap START=KEY_ENTER --ui-buttonmap DD=REL_WHEEL:1:150 --ui-buttonmap DU=REL_WHEEL:-1:150 --trigger-as-button --deadzone 4000 --wid 0 --led 4 --quiet --silent & sleep 1
+         --ui-buttonmap LT=BTN_RIGHT,RT=BTN_LEFT,x=KEY_E \
+         --ui-buttonmap A=KEY_SPACE --ui-buttonmap B=KEY_LEFTSHIFT --ui-buttonmap X=KEY_E --ui-buttonmap BACK=KEY_ESC --ui-buttonmap START=KEY_ENTER --ui-buttonmap LB=REL_WHEEL:1:150 --ui-buttonmap RB=REL_WHEEL:-1:150 --deadzone 4000 --wid 0 --led 4 --quiet --silent & sleep 1
         sleep .2
 
 /opt/retropie/supplementary/runcommand/runcommand.sh 0 "xinit \"/opt/retropie/ports/minecraft/Minecraft.sh\"" minecraft
