9dfd89383e2fa11aa99bd554110cf7e489432f2c
pancakeykakes <thephoenixproject89@gmail.com>
2015-11-14 00:24:41 -0500

    Update Minecraft TriPi Port.sh

--- Minecraft TriPi Port.sh
+++ Minecraft TriPi Port.sh
@@ -4,8 +4,8 @@ sudo killall xboxdrv
         sleep .2
          xboxdrv \
          --ui-axismap x2=REL_X:10,y2=REL_Y:10,x1=KEY_LEFT:KEY_RIGHT,y1=KEY_UP:KEY_DOWN,x1=KEY_A:KEY_D,y1=KEY_W:KEY_S \
-         --ui-buttonmap LT=BTN_RIGHT,RT=BTN_LEFT,x=KEY_E \
-         --ui-buttonmap A=KEY_SPACE --ui-buttonmap B=KEY_LEFTSHIFT --ui-buttonmap X=KEY_E --ui-buttonmap BACK=KEY_ESC --ui-buttonmap START=KEY_ENTER --ui-buttonmap LB=REL_WHEEL:1:150 --ui-buttonmap RB=REL_WHEEL:-1:150 --ui-buttonmap GUIDE=exec:/home/pi/killmc.sh --ui-buttonmap DD=KEY_DOWN --ui-buttonmap DU=KEY_UP --ui-buttonmap DL=KEY_LEFT --ui-buttonmap DR=KEY_RIGHT --deadzone 4000 --wid 0 --led 4 --quiet --silent & sleep 1
+         --ui-buttonmap LT=BTN_RIGHT,RT=BTN_LEFT,y=KEY_E \
+         --ui-buttonmap A=KEY_SPACE --ui-buttonmap B=KEY_LEFTSHIFT --ui-buttonmap Y=KEY_E --ui-buttonmap BACK=KEY_ESC --ui-buttonmap START=KEY_ENTER --ui-buttonmap LB=REL_WHEEL:1:150 --ui-buttonmap RB=REL_WHEEL:-1:150 --ui-buttonmap GUIDE=exec:/home/pi/killmc.sh --ui-buttonmap DD=KEY_DOWN --ui-buttonmap DU=KEY_UP --ui-buttonmap DL=KEY_LEFT --ui-buttonmap DR=KEY_RIGHT --deadzone 4000 --wid 0 --led 4 --quiet --silent & sleep 1
          sleep .2
 
 /opt/retropie/supplementary/runcommand/runcommand.sh 0 "xinit \"/opt/retropie/ports/minecraft/Minecraft.sh\"" minecraft
