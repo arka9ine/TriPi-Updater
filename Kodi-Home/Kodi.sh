0413d3afee98a3c5d84fee34ca7e3ca53187abe1
pancakeykakes <thephoenixproject89@gmail.com>
2015-12-27 21:52:34 -0500

    Update Kodi.sh

--- Kodi-Home/Kodi.sh
+++ Kodi-Home/Kodi.sh
@@ -2,12 +2,7 @@ while pgrep omxplayer &>/dev/null;
     do sleep 1;
 done
 
-[ "`tty`" = "/dev/tty1" ] && startkodi 
-
-sudo chmod +x /dev/uinput
-
-xboxdrv --detach-kernel-driver --wid 0 --led 3 --mimic-xpad --trigger-as-button --autofire LT=75 --autofire RT=75 --autofire DD=150 --autofire DU=150 --autofire DL=150 --autofire DR=150 --ui-buttonmap LT=KEY_F9 --ui-buttonmap RT=KEY_F10 --ui-buttonmap DD=KEY_DOWN --ui-buttonmap DU=KEY_UP --ui-buttonmap DL=KEY_LEFT --ui-buttonmap DR=KEY_RIGHT --ui-buttonmap A=KEY_ENTER --ui-buttonmap B=KEY_BACKSPACE --deadzone 80% --quiet --silent & sleep 1
-        sleep .2
+[ "`tty`" = "/dev/tty1" ] && startkodi && sudo chmod +x /dev/uinput && xboxdrv --detach-kernel-driver --wid 0 --led 3 --mimic-xpad --trigger-as-button --autofire LT=75 --autofire RT=75 --autofire DD=150 --autofire DU=150 --autofire DL=150 --autofire DR=150 --ui-buttonmap LT=KEY_F9 --ui-buttonmap RT=KEY_F10 --ui-buttonmap DD=KEY_DOWN --ui-buttonmap DU=KEY_UP --ui-buttonmap DL=KEY_LEFT --ui-buttonmap DR=KEY_RIGHT --ui-buttonmap A=KEY_ENTER --ui-buttonmap B=KEY_BACKSPACE --deadzone 80% --quiet --silent & sleep 1
 
 wait kodi.bin
 
