abd5da97b199102372f19920a9a6f6b5eddc2ec8
pancakeykakes <thephoenixproject89@gmail.com>
2016-02-03 20:58:58 -0500

    Update Enable Auto-Update.sh

--- Enable Auto-Update.sh
+++ Enable Auto-Update.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+echo -e "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n"
+
+echo "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
+echo "$(tput setaf 2)                        _______     _  _____   _"
+echo "                       |__   __|   (_)|  __ \ (_)  "
+echo "                          | | _ __  _ | |__) | _   "
+echo "                          | || '__|| ||  ___/ | |  "
+echo "                          | || |   | || |     | |  "
+echo "                          |_||_|   |_||_|     |_|  "
+echo
+echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
+echo
 echo "$(tput setaf 2)This will enable auto-updating of TriPi every night at midnight."
+echo
 echo "You will lose customizations to certain configs such as Retroarch.cfg, and smb.conf, as they will be replaced with up-to-date defaults."
+echo
 echo "If you do not want TriPi defaults, you probably should not do this."
+echo
 echo "If you decide later you no longer want updates automatically, kill this script or just run thr Disable Auto-Update option."
+echo
 echo "Starting in 20 seconds...$(tput sgr 0)"
 sleep 20
 
