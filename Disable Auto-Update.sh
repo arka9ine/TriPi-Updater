04e939b6b9c6659eaccb4e43527037f82187e5e2
pancakeykakes <thephoenixproject89@gmail.com>
2016-02-03 21:22:17 -0500

    Update Disable Auto-Update.sh

--- Disable Auto-Update.sh
+++ Disable Auto-Update.sh
@@ -12,13 +12,9 @@ echo "                          |_||_|   |_||_|     |_|  "
 echo
 echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
 echo
-echo "$(tput setaf 2)This will enable auto-updating of TriPi every night at midnight."
+echo "$(tput setaf 2)This will disable auto-updating of TriPi every night at midnight."
 echo
-echo "You will lose customizations to certain configs such as Retroarch.cfg, and smb.conf, as they will be replaced with up-to-date defaults."
-echo
-echo "If you do not want TriPi defaults, you probably should not do this."
-echo
-echo "If you decide later you no longer want updates automatically, kill this script or just run thr Disable Auto-Update option."
+echo "You can always re-enable this if you change your mind."
 echo
 echo "Starting in 20 seconds...$(tput sgr 0)"
 sleep 20
