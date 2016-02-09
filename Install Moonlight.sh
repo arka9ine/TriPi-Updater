2b0a809d6d64cf0954d2ec208d79869a0639c4b2
pancakeykakes <thephoenixproject89@gmail.com>
2016-02-08 19:17:30 -0500

    Update Install Moonlight.sh
    Added fancy text.

--- Install Moonlight.sh
+++ Install Moonlight.sh
@@ -12,7 +12,7 @@ echo
 echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
 
 echo -e "$(tput setaf 2)Installing cmake...$(tput sgr 0)"
-sleep 1
+sleep 5
 sudo wget https://cmake.org/files/v3.5/cmake-3.5.0-rc1.tar.gz
 sleep 1
 tar zxf cmake-3.5.0-rcl.tar.gz
@@ -29,7 +29,7 @@ cd
 
 
 echo "$(tput setaf 2)Installing some extra libraries that we need...$(tput sgr 0)"
-sleep 1
+sleep 5
 sudo apt-get -y install libopus-dev libexpat1-dev libssl-dev libasound2-dev libudev-dev libavahi-client-dev libcurl4-openssl-dev libjs-jquery
 sleep 1
 wget http://archive.raspbian.org/raspbian/pool/main/libe/libevdev/libevdev-dev_1.4.6%2bdfsg-1_armhf.deb
@@ -43,7 +43,7 @@ sleep 1
 cd
 
 echo "$(tput setaf 2)Building and installing Moonlight...$(tput sgr 0)"
-sleep 1
+sleep 5
 wget https://github.com/irtimmer/moonlight-embedded/releases/download/v2.1.2/moonlight-embedded-2.1.2.tar.xz
 sleep 1
 tar xf moonlight-embedded-2.1.2.tar.xz
