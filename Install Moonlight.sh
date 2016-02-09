5dbdbb5d53841a9eeede26c8c29607cfd4389ce5
pancakeykakes <thephoenixproject89@gmail.com>
2016-02-09 13:25:57 -0500

    Update Install Moonlight.sh
    Added "cd" to beginning of script to make sure everything runs from home directory.

--- Install Moonlight.sh
+++ Install Moonlight.sh
@@ -13,6 +13,8 @@ echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00
 
 echo -e "$(tput setaf 2)Installing cmake...$(tput sgr 0)"
 sleep 5
+cd
+sleep 1
 sudo wget https://cmake.org/files/v3.5/cmake-3.5.0-rc1.tar.gz
 sleep 1
 tar zxf cmake-3.5.0-rcl.tar.gz
