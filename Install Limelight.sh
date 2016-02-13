987d1dd2345740e1eb4463f5a11d4e2a1490c42e
pancakeykakes <thephoenixproject89@gmail.com>
2016-02-12 21:18:21 -0500

    Update Install Limelight.sh

--- Install Limelight.sh
+++ Install Limelight.sh
@@ -12,11 +12,11 @@ echo "                          |_||_|   |_||_|     |_|  "
 echo
 echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n$(tput sgr 0)"
 
-
+echo
+echo
+echo
 echo "$(tput setaf 2)Installing java8...$(tput sgr 0)"
-echo 
-echo
-echo
+
 sleep 5
 
 sudo apt-get update && sudo apt-get -y install oracle-java8-jdk && sudo apt-get -y install input-utils
@@ -33,7 +33,7 @@ sudo mkdir /home/pi/RetroPie/roms/limelight
 echo
 echo "$(tput setaf 2)Installing Limelight config file to TriPi Menu....$(tput sgr 0)"
 
-cp /home/pi/TriPi-Updater/Configure\ Limelight.sh /home/pi/RetroPie/roms/limelight/ 
+sudo cp /home/pi/TriPi-Updater/Configure\ Limelight.sh /home/pi/RetroPie/roms/limelight/ 
 
 echo
 echo "$(tput setaf 2)Downloading Limelight...$(tput sgr 0)"
@@ -48,19 +48,24 @@ clear
 
 read -p "Installing limelight menu to Emulation Station, press anykey to continue `echo $'\n> '`" -n1 -s
 sudo sed -i -e 's|</systemList>|<system>\n<name>limelight</name>\n<fullname>Limelight</fullname>\n<path>~/RetroPie/roms/limelight</path>\n<extension>.sh .SH</extension>\n<command>bash %ROM%</command>\n<platform>limelight</platform>\n<theme>limelight</theme>\n</system>\n</systemList>|g' /etc/emulationstation/es_systems.cfg
+
+sudo rm -rf /etc/emulationstation/themes/simple/limelight
+sudo rm -rf /etc/emulationstation/themes/simple/limelight/art/
 sudo mkdir /etc/emulationstation/themes/simple/limelight
 sudo mkdir /etc/emulationstation/themes/simple/limelight/art/
-cd /etc/emulationstation/themes/simple/limelight
-sudo wget  https://github.com/stsfin/RetropieLimelightInstaller/releases/download/1.3.1/theme.xml
-cd /etc/emulationstation/themes/simple/limelight/art/
+
+cd /home/pi/limelight
+sudo wget https://github.com/stsfin/RetropieLimelightInstaller/releases/download/1.3.1/theme.xml
 sudo wget https://github.com/stsfin/RetropieLimelightInstaller/releases/download/1.3.1/limelight.png
 sudo wget https://github.com/stsfin/RetropieLimelightInstaller/releases/download/1.3.1/limelight_art.png
 sudo wget https://github.com/stsfin/RetropieLimelightInstaller/releases/download/1.3.1/limelight_art_blur.png
 
-cd /home/pi/RetroPie/roms/limelight
+sudo cp theme.xml /etc/emulationstation/themes/simple/limelight
+sudo cp {limelight.png,limelight_art.png,limelight_art_blur.png} /etc/emulationstation/themes/simiple/limelight/art
 
 echo "$(tput setaf 2)Installing additional Limelight scripts...$(tput sgr 0)"
 
+cd /home/pi/RetroPie/roms/limelight
 
 sudo echo "#!/bin/bash" > limelight720p60fps.sh
 sudo echo "cd /home/pi/limelight/ && java -jar limelight.jar stream -720 -60fps "$ip" -app Steam -mapping mapfile.map" >>  limelight720p60fps.sh
