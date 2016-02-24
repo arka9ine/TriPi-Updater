6d24de4a2588dfd0a3349496c6b25f68f03d54b6
pancakeykakes <thephoenixproject89@gmail.com>
2016-02-23 22:06:47 -0500

    Updated to Version 4.6 - Code Cleanup

---
+++ Ports/kodi.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo killall xboxdrv
+        sleep .2
+        xboxdrv --detach-kernel-driver --wid 0 --led 3 --mimic-xpad --trigger-as-button --autofire LT=75 --autofire RT=75 --autofire DD=150 --autofire DU=150 --autofire DL=150 --autofire DR=150 --ui-buttonmap LT=KEY_F9 --ui-buttonmap RT=KEY_F10 --ui-buttonmap DD=KEY_DOWN --ui-buttonmap DU=KEY_UP --ui-buttonmap DL=KEY_LEFT --ui-buttonmap DR=KEY_RIGHT --ui-buttonmap A=KEY_ENTER --ui-buttonmap B=KEY_BACKSPACE --deadzone 80% --quiet --silent & sleep 1
+        sleep .2
+/opt/retropie/supplementary/runcommand/runcommand.sh 0 "startkodi" 
+
+wait kodi.bin
+
+sudo killall xboxdrv
+sleep .2
+xboxdrv --trigger-as-button --wid 0 --led 2 --detach-kernel-driver --quiet --silent & sleep 1
+sleep .2
