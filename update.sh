1712fa10b801f38d732febfd12fbd250e7da083f
pancakeykakes <thephoenixproject89@gmail.com>
2016-02-24 09:53:33 -0500

    Updated to Version 4.6 - Code Cleanup

--- update.sh
+++ update.sh
@@ -8,17 +8,20 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
+echo "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
 echo
-echo -e "$(tput setaf 2)Hi!\n"
-echo -e "This is the TriPi updater script."
 echo
-echo -e "I am going to install updates to your TriPi environment.\n"
-echo "This could potentially break things. If you have any issues, consult https://tripi.junaos.com or submit an issue to github."
+echo "$(tput setaf 2)Hi!"
+echo "This is the TriPi updater script."
+echo
+echo "I am going to install updates to your TriPi environment."
+echo
+echo "This could potentially break things. If you have any issues, consult tripi.junaos.com or submit an issue to github."
 echo "Not all updates are the same, so check the README.md file to find out if this is a stable release!"
 echo
 echo "!!!!WARNING!!!! This Update is replacing RETROARCH.CFG, which means you may need to reconfigure any custom settings after this is done!"
-echo -e "I will begin in 20 seconds...\n\n"
+echo
+echo "I will begin in 20 seconds..."
 
 sleep 20
 
@@ -33,7 +36,8 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
+echo "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
+echo
 echo
 echo "$(tput setaf 2)Checking xboxdrv... $(tput sgr 0)"
 sleep 2
@@ -50,9 +54,10 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\\n"
+echo  "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
 echo
-echo -e "\n$(tput setaf 2)Removing old files and creating required directories...$(tput sgr 0)"
+echo
+echo  "$(tput setaf 2)Removing old files and creating required directories...$(tput sgr 0)"
 sleep 2
 
 mkdir /home/pi/RetroPie/roms/TriPi
@@ -92,9 +97,10 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
+echo  "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
 echo
-echo -e "\n$(tput setaf 2)Setting Controller Keybinds for Minecraft-Pi... $(tput sgr 0)"
+echo
+echo  "$(tput setaf 2)Setting Controller Keybinds for Minecraft-Pi... $(tput sgr 0)"
 sleep 2
 
 cp /home/pi/TriPi-Updater/Ports/Minecraft\ TriPi\ Port.sh /home/pi/RetroPie/roms/ports
@@ -108,9 +114,10 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
+echo  "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
 echo
-echo -e "\n$(tput setaf 2)Setting permissions for new files...$(tput sgr 0)"
+echo
+echo  "$(tput setaf 2)Setting permissions for new files...$(tput sgr 0)"
 sleep 2
 
 
@@ -130,22 +137,20 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
+echo  "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
 echo
-echo -e "\n$(tput setaf 2)Done!\n" 
+echo
+echo  "$(tput setaf 2)Done!" 
 
 echo "I will reboot your Raspberry Pi in 10 seconds. ENJOY! -Pancakeykakes$(tput sgr 0)"
 
-SECONDS_TILL_REBOOT=10
 
 SECONDS_TILL_REBOOT=10
 while [[ ${SECONDS_TILL_REBOOT} -gt 0 ]]; do
   sleep 1
   SECONDS_TILL_REBOOT=$[${SECONDS_TILL_REBOOT} -1]
-  if [[ $((${SECONDS_TILL_REBOOT} % 5)) -eq 0 ]]; then
+  if [[ $((${SECONDS_TILL_REBOOT} % 5)) q 0 ]]; then
     echo "${SECONDS_TILL_REBOOT} seconds until reboot"
   fi
 done
 sudo reboot
-
-}
