cb7aa75077be845c871f4db6af69a70b17009065
pancakeykakes <thephoenixproject89@gmail.com>
2016-02-24 09:41:49 -0500

    Updated to Version 4.6 - Code Cleanup
    More fixes from that stupid text editor I mistakenly tried to use.

--- update.sh
+++ update.sh
@@ -8,17 +8,17 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\\n"
+echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
 echo
-echo -e "$(tput setaf 2)Hi!\\n"\
-echo -e "This is the TriPi updater script."\
-echo\
-echo -e "I am going to install updates to your TriPi environment.\\n"\
+echo -e "$(tput setaf 2)Hi!\n"
+echo -e "This is the TriPi updater script."
+echo
+echo -e "I am going to install updates to your TriPi environment.\n"
 echo "This could potentially break things. If you have any issues, consult https://tripi.junaos.com or submit an issue to github."
 echo "Not all updates are the same, so check the README.md file to find out if this is a stable release!"
 echo
 echo "!!!!WARNING!!!! This Update is replacing RETROARCH.CFG, which means you may need to reconfigure any custom settings after this is done!"
-echo -e "I will begin in 20 seconds...\\n\\n"
+echo -e "I will begin in 20 seconds...\n\n"
 
 sleep 20
 
@@ -33,7 +33,7 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\\n"
+echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
 echo
 echo "$(tput setaf 2)Checking xboxdrv... $(tput sgr 0)"
 sleep 2
@@ -52,7 +52,7 @@ echo "                          |_||_|   |_||_|     |_|  "
 echo
 echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\\n"
 echo
-echo -e "\\n$(tput setaf 2)Removing old files and creating required directories...$(tput sgr 0)"
+echo -e "\n$(tput setaf 2)Removing old files and creating required directories...$(tput sgr 0)"
 sleep 2
 
 mkdir /home/pi/RetroPie/roms/TriPi
@@ -76,12 +76,12 @@ cp /home/pi/TriPi-Updater/System/retroarch.cfg /opt/retropie/configs/all/
 cp /home/pi/TriPi-Updater/Ports/kodi.sh /home/pi/RetroPie/roms/ports
 cp /home/pi/TriPi-Updater/Ports/killmc.sh /home/pi/
 
-cp Manual\\ Update.sh /home/pi/RetroPie/roms/TriPi
+cp Manual\ Update.sh /home/pi/RetroPie/roms/TriPi
 
 cp /home/pi/TriPi-Updater/TriPi-Menu/theme.xml /etc/emulationstation/themes/simple-dark/TriPi
 cp -r /home/pi/TriPi-Updater/TriPi-Menu/art /etc/emulationstation/themes/simple-dark/TriPi
 
-cp /home/pi/TriPi-Updater/Limelight/Install\\ Limelight.sh /home/pi/RetroPie/roms/TriPi
+cp /home/pi/TriPi-Updater/Limelight/Install\ Limelight.sh /home/pi/RetroPie/roms/TriPi
 
 clear
 echo "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
@@ -92,12 +92,12 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\\n"
+echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
 echo
-echo -e "\\n$(tput setaf 2)Setting Controller Keybinds for Minecraft-Pi... $(tput sgr 0)"
+echo -e "\n$(tput setaf 2)Setting Controller Keybinds for Minecraft-Pi... $(tput sgr 0)"
 sleep 2
 
-cp /home/pi/TriPi-Updater/Ports/Minecraft\\ TriPi\\ Port.sh /home/pi/RetroPie/roms/ports
+cp /home/pi/TriPi-Updater/Ports/Minecraft\ TriPi\ Port.sh /home/pi/RetroPie/roms/ports
 
 clear
 echo "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000"
@@ -108,13 +108,13 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\\n"
+echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
 echo
-echo -e "\\n$(tput setaf 2)Setting permissions for new files...$(tput sgr 0)"
+echo -e "\n$(tput setaf 2)Setting permissions for new files...$(tput sgr 0)"
 sleep 2
 
 
-chmod +x \{/home/pi/RetroPie/roms/TriPi/Install\\ Limelight.sh,/home/pi/killmc.sh,/home/pi/RetroPie/roms/ports/kodi.sh,/home/pi/RetroPie/roms/ports/Minecraft\\ TriPi\\ Port.sh,/home/pi/RetroPie/roms/TriPi/Manual\\ Update.sh,/etc/profile.d/emu.sh,/etc/rc.local\}
+chmod +x {/home/pi/RetroPie/roms/TriPi/Install\ Limelight.sh,/home/pi/killmc.sh,/home/pi/RetroPie/roms/ports/kodi.sh,/home/pi/RetroPie/roms/ports/Minecraft\ TriPi\ Port.sh,/home/pi/RetroPie/roms/TriPi/Manual\ Update.sh,/etc/profile.d/emu.sh,/etc/rc.local\}
 chmod a+x /etc/init.d/asplashscreen
 insserv /etc/init.d/asplashscreen
 chmod +x /etc/emulationstation/es_systems.cfg
@@ -130,22 +130,22 @@ echo "                          | || '__|| ||  ___/ | |  "
 echo "                          | || |   | || |     | |  "
 echo "                          |_||_|   |_||_|     |_|  "
 echo
-echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\\n"
+echo -e "$(tput setaf 1)01010100 01110010 01101001 01010000 01101001 00110010 00101110 00110000\n"
 echo
-echo -e "\\n$(tput setaf 2)Done!\\n" 
+echo -e "\n$(tput setaf 2)Done!\n" 
 
 echo "I will reboot your Raspberry Pi in 10 seconds. ENJOY! -Pancakeykakes$(tput sgr 0)"
 
 SECONDS_TILL_REBOOT=10
 
-while [[ $\{SECONDS_TILL_REBOOT\} -gt 0 ]]; do
+SECONDS_TILL_REBOOT=10
+while [[ ${SECONDS_TILL_REBOOT} -gt 0 ]]; do
   sleep 1
-  SECONDS_TILL_REBOOT=$[$\{SECONDS_TILL_REBOOT\} -1]
-  if [[ $(($\{SECONDS_TILL_REBOOT\} % 5)) -eq 0 ]]; then
-    echo "$\{SECONDS_TILL_REBOOT\} seconds until reboot"
+  SECONDS_TILL_REBOOT=$[${SECONDS_TILL_REBOOT} -1]
+  if [[ $((${SECONDS_TILL_REBOOT} % 5)) -eq 0 ]]; then
+    echo "${SECONDS_TILL_REBOOT} seconds until reboot"
   fi
 done
-
-reboot
+sudo reboot
 
 }
